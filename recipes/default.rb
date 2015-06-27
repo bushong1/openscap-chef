62d7bbd12b86dbce80b08f49e7829e54506f929b
Charles Bushong <cbushong@fearsol.com>
2015-06-27 10:45:01 -0400

    Adding rubocop, breaking out versions

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,46 +1,71 @@
 #
 # Cookbook Name:: openscap-chef
 # Recipe:: default
-#
-# Copyright (c) 2015 ADV, All Rights Reserved.
-
 # Include OS platform speciic package installs
+
 begin
   include_recipe "openscap-chef::_#{node['platform_family']}"
 rescue Chef::Exceptions::RecipeNotFound
   Chef::Log.error <<-EOSTDOUT
-An adv_base recipe does not exist for '#{node['platform_family']}'. This
-means the adv_base cookbook does not have support for the
+An openscap recipe does not exist for '#{node['platform_family']}'. This
+means the openscap cookbook does not have support for the
 #{node['platform_family']} family.
 EOSTDOUT
 end
 
 directory 'openscap directory' do
-  path        node['openscap']['directory']
-  owner       'root'
-  group       'root'
-  mode        '0755'
-  recursive   true
-  action      :create
+  path node['openscap']['directory']
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
+  action :create
 end
 
 directory 'scap-security-guide directory' do
-  path        node['scap_security_guide']['directory']
-  owner       'root'
-  group       'root'
-  mode        '0755'
-  recursive   true
-  action      :create
+  path node['scap_security_guide']['directory']
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
+  action :create
 end
 
 git 'openscap' do
-  repository   node['openscap']['git_repository']
-  destination  node['openscap']['directory']
-  action       :sync
+  repository node['openscap']['git_repository']
+  destination node['openscap']['directory']
+  action :sync
 end
 
 git 'openscap-security-guide' do
-  repository   node['scap_security_guide']['git_repository']
-  destination  node['scap_security_guide']['directory']
-  action       :sync
+  repository node['scap_security_guide']['git_repository']
+  destination node['scap_security_guide']['directory']
+  action :sync
+end
+
+# TODO: Determine most appropriate way to split this into different recipes while staying DRY
+open_scap_guide_version = ''
+case node['platform_family']
+when 'rhel'
+  if    node['platform_version'].match(/^5/)
+    open_scap_guide_version = 'rhel5'
+  elsif node['platform_version'].match(/^6/)
+    open_scap_guide_version = 'rhel6'
+  elsif node['platform_version'].match(/^7/)
+    open_scap_guide_version = 'rhel7'
+  else
+    Chef::Log.error "This cookbook is not yet configured for '#{node['platform_family']} v#{node['platform_version']}'."
+  end
+else
+  Chef::Log.error "This cookbook is not yet configured for '#{node['platform_family']}'."
+end
+
+# TODO: Determine most appropriate way to split this into different recipes while staying DRY
+bash 'make_openscap_guide' do
+  user 'root'
+  cwd node['scap_security_guide']['directory']
+  code <<-EOH
+    make #{open_scap_guide_version}
+  EOH
+  action :run
 end
